0f03a1443596a2c2decf30dd5b0532b4afaf4e69
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-06-25 10:35:03 +0530

    auth

--- ass1/assignment1.js
+++ ass1/assignment1.js
@@ -1,17 +1,21 @@
 import express from 'express'
  const app = express()
 
- let requestCount = 0;
- function customMiddleware(req, res, next){
-    requestCount += 1
-    console.log(req.method)
-    console.log(req.url)
-    console.log("count="+requestCount)
-    next()
- }
- app.use(customMiddleware)
+//  let requestCount = 0;
+//  function customMiddleware(req, res, next){
+//     requestCount += 1
+//     console.log(req.method)
+//     console.log(req.url)
+//     console.log("count="+requestCount)
+//     next()
+//  }
+//  app.use(customMiddleware)
  app.get('/', (req, res) => {
-    console.log("Hii")
+    res.send("Hii")
+ })
+
+  app.get('/', (req, res) => {
+    res.send("There")
  })
 
  app.listen(3000)
