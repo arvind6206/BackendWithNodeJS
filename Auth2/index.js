6cabd9f1ea8e6d703d310e95ff45004f6bcfac03
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-06-25 14:14:33 +0530

    some more files

--- Auth2/index.js
+++ Auth2/index.js
@@ -1,12 +1,22 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 
+import path from 'path'
+import {fileURLToPath} from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const JWT_SECRET = "huy^ft%dr*gu"
 
 const app = express()
 app.use(express.json())
 
 const users = []
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+})
 
 app.post('/signup', (req, res) => {
     const username = req.body.username
