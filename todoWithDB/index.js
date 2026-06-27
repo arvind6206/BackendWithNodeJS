0b39690a378de46df5aa858c484a67d9a829905a
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-06-27 10:00:17 +0530

    db

---
+++ todoWithDB/index.js
@@ -0,0 +1,81 @@
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import { UserModel } from "./models/User.js";
+
+const app = express();
+app.use(express.json());
+
+app.post("/signup", async function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    await UserModel.create({
+        email: email,
+        password: password,
+        name: name
+    });
+    
+    res.json({
+        message: "You are signed up"
+    })
+
+    
+});
+
+
+app.post("/signin", function(req, res) {
+      const email = req.body.email;
+    const password = req.body.password;
+
+    const response = await UserModel.findOne({
+        email: email,
+        password: password,
+    });
+
+    if (response) {
+        const token = jwt.sign({
+            id: response._id.toString()
+        })
+
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrect creds"
+        })
+    }
+});
+
+function auth(req, res, next) {
+    const token = req.headers.authorization;
+
+    const response = jwt.verify(token, JWT_SECRET);
+
+    if (response) {
+        req.userId = token.userId;
+        next();
+    } else {
+        res.status(403).json({
+            message: "Incorrect creds"
+        })
+    }
+}
+
+module.exports = {
+    auth,
+    JWT_SECRET
+}
+
+app.post("/todo", function(req, res) {
+
+});
+
+
+app.get("/todos", function(req, res) {
+
+});
+
+app.listen(3000);
